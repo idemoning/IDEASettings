eb976812632f9117ef73e43805ea01929327002d
idemoning <idemoning@163.com>
2021-05-15 16:40:49 +0800

    IU-2020.2.4 <idemon@192.168.1.5 Create baseRefactoring.xml, ide.general.xml, project.default.xml, diff.xml, Enum.java, Interface.java, lombok-plugin.xml, git.xml, profiler.xml, Default.xml, code.style.schemes, find.xml, filetypes.xml, colors.scheme.xml, path.macros.xml, databaseDrivers.xml, github.xml, aws.xml, javarebel.xml, Class.java, jdk.table.xml, lightEdit.xml, laf.xml, vcs.xml, toStringTemplates.xml, mybatis.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+* @author ${USER}
+* @date ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+* @desc 
+*/
+public enum ${NAME} {
+}
